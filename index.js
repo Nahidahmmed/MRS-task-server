53c972cd833d8a5879bf0dbc0f976887ac0d057c
Nahidahmmed <nahidahmmed411@gmail.com>
2023-08-03 22:21:59 +0600

    minimum work done

--- index.js
+++ index.js
@@ -27,14 +27,30 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const taskCollection = client.db("TaskDB").collection("Tasks");
+
 
         app.get('/tasks', async(req,res) =>{
             const result = await taskCollection.find().toArray();
             res.send(result);
         })
+
+        app.post('/tasks', async(req, res) =>{
+            const newItem = req.body;
+            const result = await taskCollection.insertOne(newItem);
+            res.send(result);
+        })
+
+      
+
+        // app.delete('/tasks/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: new ObjectId(id) };
+        //     const result = await taskCollection.deleteOne(query);
+        //     res.send(result);
+        //   })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
